23a0732e1a1db879326c1f3492279aeb194553a8
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-10-30 03:01:16 +0300

    added calendar implementation
    added navigation component dependenct
    added nav graph and connected it with bottom navigation

--- app/src/main/java/com/example/remap/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/remap/ui/activities/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.remap.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.remap.R
 import com.example.remap.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        navView.setupWithNavController(navController)
     }
 }
